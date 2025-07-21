5ffc20d2d76fdd20b57594c51bdbbbc308f82e85
Ingryd <ingrydcduarte@gmail.com>
2025-07-21 18:23:19 -0300

    chore: cleaning dataset routes

--- server/src/routes/datasetsRoutes.js
+++ server/src/routes/datasetsRoutes.js
@@ -2,22 +2,14 @@ import express from 'express';
 import uploadConfig from '../utils/uploadConfig.js';
 import { uploadDataset } from '../controllers/datasetController.js';
 import { authMiddleware } from '../middleware/authMiddleware.js';
+import * as datasetModel from '../models/datasetModel.js';
 
 const router = express.Router();
 
 router.use(authMiddleware);
-router.get('/', async (req, res) => {
-  res.status(200).json({ message: "Listando datasets" });
-});
+router.get('/', authMiddleware, datasetModel.listarDataset);
 
-router.post('/upload', uploadConfig.single('file'), uploadDataset);
-
-router.get('/:id/records', async (req, res) => {
-  const { id } = req.params;
-  const records = await prisma.records.findMany({
-    where: { dataset_id: id },
-  });
-  res.status(200).json(records);
-});
+router.post('/upload', authMiddleware, uploadConfig.single('file'), uploadDataset);
+router.get('/:id/records', authMiddleware, datasetModel.listarRecordsPorDataset);
 
 export default router;
