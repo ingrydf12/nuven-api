4a258cc5f20cb418a593ed276b4fdb95d548baea
Ingryd <ingrydcduarte@gmail.com>
2025-07-21 18:24:04 -0300

    feat: records model (prisma)

---
+++ server/src/models/recordsModel.js
@@ -0,0 +1,32 @@
+import prisma from "../database/prisma.js";
+
+export const listarRecords = async (req, res) => {
+  try {
+    const records = await prisma.records.findMany();
+    res.status(200).json(records);
+    }
+    catch (error) { 
+    console.error("Erro ao listar registros:", error);
+    res.status(500).json({ error: "Erro ao listar registros" });
+    }
+};
+
+export const listaRecordsPorQuery = async (query) => {
+  try {
+    const records = await prisma.records.findMany({
+      where: {
+        OR: [
+          { dados_json: { contains: query, mode: 'insensitive' } },
+          { dataset: { nome: { contains: query, mode: 'insensitive' } } }
+        ]
+      },
+      include: {
+        dataset: true
+      }
+    });
+    return records;
+  } catch (error) {
+    console.error("Erro ao listar registros:", error);
+    throw error;
+  }
+}
