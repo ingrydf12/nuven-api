5452b3c112f0f2ba42f022e71015ec43df9c6d8d
Ingryd <ingrydcduarte@gmail.com>
2025-07-20 22:12:04 -0300

    setup: project dependencies and basic server

---
+++ server/src/server.js
@@ -0,0 +1,20 @@
+import express from 'express';
+import authRoutes from './routes/authRoutes.js';
+import prisma from './database/prisma.js';
+import datasetsRoutes from './routes/datasetsRoutes.js';
+
+const app = express();
+app.use(express.json());
+
+const PORT = process.env.PORT || 3000;
+
+app.use('/', authRoutes);
+app.use('/datasets', datasetsRoutes);
+
+app.get('/', (_req, res) => {
+  res.send('NUVEN API - Desafio técnico, Ingryd.');
+});
+
+app.listen(PORT, () => {
+  console.log(`Servidor rodando na porta ${PORT}`);
+});
