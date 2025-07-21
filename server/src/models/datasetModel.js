0715945f2b1b21d44e85cf45fc2ab404f0db72fe
Ingryd <ingrydcduarte@gmail.com>
2025-07-21 18:23:28 -0300

    feat: dataset list records

--- server/src/models/datasetModel.js
+++ server/src/models/datasetModel.js
@@ -1,4 +1,4 @@
-import prisma from "../database/prisma";
+import prisma from "../database/prisma.js";
 
 export const listarDataset = async (req, res) => {
   try {
@@ -28,3 +28,25 @@ export const envioArquivo = async (req, res) => {
         res.status(500).json({ error: "Erro ao enviar arquivo" });
     }
 }
+
+export const listarRecordsPorDataset = async (req, res) => {
+  const { id } = req.params;
+
+  try {
+    let records = await prisma.records.findMany({
+      where: { dataset_id: Number(id) },
+    });
+
+    records = records.map(record => ({
+      ...record,
+      dados_json: typeof record.dados_json === 'string' 
+        ? JSON.parse(record.dados_json) 
+        : record.dados_json
+    }));
+
+    res.status(200).json(records);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: 'Erro ao buscar registros' });
+  }
+};
